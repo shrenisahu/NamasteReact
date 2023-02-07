817816a75f7fe7e3a0630d69451a8bde21b62758
shrenisahu@gmail.com <shrenisahu@gmail.com>
2023-02-07 22:28:01 +0530

    day 4

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title", // the h1 tag will have id as title
-  },
-  "namaste parcel"
-);
-
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title2",
-  },
-  "javascript"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading1, heading2]
-);
-
+import Header from "./Component/Header";
+import Body from "./Component/Body";
+import Footer from "./Component/Footer";
+import "./index.css";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<App />);
